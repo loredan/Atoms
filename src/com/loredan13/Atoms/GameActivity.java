5c1f0ff23a614b069e256fcef58180705635605f
Loredan <loredan725@gmail.com>
2014-07-25 18:31:32 +0400

    Начальный коммит

---
+++ src/com/loredan13/Atoms/GameActivity.java
@@ -0,0 +1,14 @@
+package com.loredan13.Atoms;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
